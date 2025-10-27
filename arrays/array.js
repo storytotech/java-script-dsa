769cde5e615994818b4da5f6083a6dd43a695794
storytotech <sochghazali@gmail.com>
2025-10-27 12:26:57 +0545

    Add array.js file

---
+++ arrays/array.js
@@ -0,0 +1,22 @@
+let math = [90,34,43,54]
+console.log(math);
+console.log(math[0]);
+
+// object 
+
+let math_with_std = {student : "hansian",age : 10,marks  : 200}
+console.log(math_with_std.marks);
+
+let math_news = {student : "hansian",age : 23,map : "north india new garden"}
+console.log(math_news.student);
+
+let newarray = new Array()
+newarray[0] = "hasnian"
+
+console.log(newarray);
+
+
+let arraynew = new Array(6).fill(1)
+console.log(arraynew);
+let arrayfill = Array.of("hasnian",23,"marks")
+console.log(arrayfill);
